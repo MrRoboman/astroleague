e6b263a73f63657d0ee10bd81f9e067a64f06420
MrRoboman <rkayson@gmail.com>
2016-05-10 16:16:23 -0700

    working on circle collision

--- rocketleague.js
+++ rocketleague.js
@@ -14,42 +14,59 @@ document.addEventListener('DOMContentLoaded', function() {
     this.width = canvas.width;
     this.height = canvas.height;
 
-    this.p0 = new Ship(this, this.width * 0.20, this.height * 0.5, 0, 0);
-    this.p1 = new Ship(this, this.width * 0.80, this.height * 0.5, Math.PI, 1);
+    this.sprites = [
+      new Ship(this, this.width * 0.20, this.height * 0.5, 0, 0),
+      new Ship(this, this.width * 0.80, this.height * 0.5, Math.PI, 1)
+    ];
 
     window.requestAnimationFrame(this.update.bind(this));
   };
 
   Game.prototype = {
 
+    collision: function(a, b) {
+      if(a.left() > b.right() && a.right() < b.left() &&
+         a.top() > b.bottom() && a.bottom() < b.top()) {
+           return false;
+      }
+      var x = a.x - b.x;
+      var y = a.y - b.y;
+      if(Math.sqrt(x*x + y*y) > a.r + b.r){
+        return false;
+      }
+
+      return true;
+    },
+
     handleInput: function() {
-      this.p0.normalize();
-      this.p1.normalize();
+      this.sprites.forEach(function(sprite) {
+        sprite.normalize();
+      });
 
       if(window.keys.LEFT) {
-        this.p1.rotateDir -= 1;
+        this.sprites[1].rotateDir -= 1;
       }
       if(window.keys.RIGHT) {
-        this.p1.rotateDir += 1;
+        this.sprites[1].rotateDir += 1;
       }
       if(window.keys.UP) {
-        this.p1.accelerate();
+        this.sprites[1].accelerate();
       }
       if(window.keys.DOWN) {
-        // this.p1.deccelerate();
+        // this.sprites[1].deccelerate();
       }
 
       if(window.keys.A) {
-        this.p0.rotateDir -= 1;
+        this.sprites[0].rotateDir -= 1;
       }
       if(window.keys.D) {
-        this.p0.rotateDir += 1;
+        this.sprites[0].rotateDir += 1;
       }
       if(window.keys.W) {
-        this.p0.accelerate();
+        this.sprites[0].accelerate();
       }
       if(window.keys.S) {
-        // this.p0.deccelerate();
+        // this.sprites[0].deccelerate();
       }
     },
 
@@ -59,13 +76,20 @@ document.addEventListener('DOMContentLoaded', function() {
       this.handleInput();
 
       //logic
-      this.p0.logic();
-      this.p1.logic();
+      this.sprites.forEach(function(sprite){
+        sprite.logic();
+      });
+
+      if(this.collision(this.sprites[0], this.sprites[1])){
+        
+      }
 
       //draw
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      this.p0.draw(ctx, shipSprite);
-      this.p1.draw(ctx, shipSprite);
+      this.sprites.forEach(function(sprite) {
+        sprite.draw(ctx, shipSprite);
+      });
+
       window.requestAnimationFrame(this.update.bind(this));
     }
   };
